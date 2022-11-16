630fda251c9b9a0e0f1d5a557c7214d81d138793
karuppasamy-chinnapandi <115548599+karuppasamy-chinnapandi@users.noreply.github.com>
2022-11-16 21:21:49 +0530

    Add files via upload

---
+++ arithmetic_operators.c
@@ -0,0 +1,16 @@
+//arithmetic operators
+#include <stdio.h>
+int main()
+{
+int a=10, b=20,c;
+c=a+b;
+printf("a+b= %d\n",c);
+c=a-b;
+printf("a-b= %d\n",c);
+c=a*b;
+printf("a*b= %d\n",c);
+c=a/b;
+printf("a/b= %d\n",c);
+c=a%b;
+printf("a%b= %d",c);
+}
