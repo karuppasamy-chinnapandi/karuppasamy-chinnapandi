b5e9fda381534e532e84a1300fce702947989c22
karuppasamy-chinnapandi <115548599+karuppasamy-chinnapandi@users.noreply.github.com>
2022-11-17 19:19:36 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int x,y;
+printf("enter the values:");
+scanf("%d%d",&x,&y);
+if(x>y){
+    printf("x is greater");
+}
+    else{
+        printf("y is greater");
+    }
+}
