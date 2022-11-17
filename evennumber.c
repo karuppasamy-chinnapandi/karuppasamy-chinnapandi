886c9dcda3e2372c27fd7902221eae8c63720e8f
karuppasamy-chinnapandi <115548599+karuppasamy-chinnapandi@users.noreply.github.com>
2022-11-17 19:19:05 +0530

    Add files via upload

---
+++ evennumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter a no to whether it is even or odd:");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("even no");
+    }
+    else{
+        printf("odd no");
+    }
+    
+}
