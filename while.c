09ef8f1d3c2a0f81a77d7fd8aff9d67b1ee318e9
karuppasamy-chinnapandi <115548599+karuppasamy-chinnapandi@users.noreply.github.com>
2022-11-21 22:09:25 +0530

    Add files via upload

---
+++ while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i,n;
+    i=0;
+    printf("Enter a no:");
+    scanf("%d",&n);
+    while(i<=n){
+        printf("The value is %d\n",i);
+        i=i+1;
+    }
+}  
