6936df23e4ed85a4e90d4a31b389cb1822ab71f9
karuppasamy-chinnapandi <115548599+karuppasamy-chinnapandi@users.noreply.github.com>
2022-11-25 23:31:24 +0530

    Add files via upload

---
+++ stringcpy.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s1[20],s2[20];
+    printf("Enter the name:");
+    scanf("%s",&s1);
+    printf("\n The string copy is %s",(s2,s1));
+}
