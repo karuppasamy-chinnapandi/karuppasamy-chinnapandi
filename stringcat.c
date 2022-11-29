8da241c1a2788d6721abb083934059e55ab5de42
karuppasamy-chinnapandi <115548599+karuppasamy-chinnapandi@users.noreply.github.com>
2022-11-29 22:04:05 +0530

    Add files via upload

---
+++ stringcat.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s1[20]="karuppasamy";
+    char s2[20]="pandi";
+    strcat(s1,s2);
+    printf("the string concatenate is :%s",s1);
+}
